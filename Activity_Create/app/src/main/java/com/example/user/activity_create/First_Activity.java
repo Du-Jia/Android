b6bad4cdcf77563acdc6d30b68ce6b93635ad15d
Du-Jia <du_jia_goodluck@163.com>
2018-10-15 15:05:30 +0800

    Add menu

--- Activity_Create/app/src/main/java/com/example/user/activity_create/First_Activity.java
+++ Activity_Create/app/src/main/java/com/example/user/activity_create/First_Activity.java
@@ -3,6 +3,8 @@ package com.example.user.activity_create;
 import android.app.AppComponentFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -31,5 +33,27 @@ public class First_Activity extends AppCompatActivity{
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+
+        }
+        return true;
     }
 }
