1d8ee51a38a102e878c4de3c471677287053d6a3
Du-Jia <du_jia_goodluck@163.com>
2018-12-10 09:20:06 +0800

    start edit by intent

--- Words_Notes/app/src/main/java/com/example/user/words_notes/EditActivity.java
+++ Words_Notes/app/src/main/java/com/example/user/words_notes/EditActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.words_notes;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -10,4 +13,21 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_new_notes);
     }
+
+    //If Button new_note was clicked, start EditActivity here
+    public static void actionStart(Context context, String folder_name){
+        Intent intent = new Intent(context, EditText.class);
+        intent.putExtra("folder_name", folder_name);
+        context.startActivity(intent);
+    }
+
+    //If notes list item was clicked or click long time, start EditActivity here
+    public static void actionStart(Context context, String title, String content){
+        Intent intent = new Intent(context, EditText.class);
+        intent.putExtra("title", title);
+        intent.putExtra("content", content);
+        context.startActivity(intent);
+    }
+
+
 }
--- Words_Notes/app/src/main/java/com/example/user/words_notes/MainActivity.java
+++ Words_Notes/app/src/main/java/com/example/user/words_notes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.words_notes;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -7,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.user.LayoutClass.Note;
 import com.example.user.LayoutClass.NoteAdapter;
@@ -18,7 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     NotesDBHelper notesdb;
     private SQLiteDatabase dbutil;
     private List<Note> noteList = null;
@@ -32,12 +37,26 @@ public class MainActivity extends AppCompatActivity {
         notesdb = new NotesDBHelper(this);
         dbutil = notesdb.getReadableDatabase();
 
+        //find all button or ui
         ListView list = (ListView)findViewById(R.id.listView);
+        Button newNotes = (Button)findViewById(R.id.new_note);
+        Button folder = (Button)findViewById(R.id.folder);
+        TextView folder_name = (TextView)findViewById(R.id.folder_name);
+
 
         noteList = getNotesList();
         NoteAdapter adapter = new NoteAdapter(MainActivity.this, R.layout.note_item, noteList);
 
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Note note = noteList.get(i);
+                EditActivity.actionStart(MainActivity.this, note.getTitle(), note.getContent());
+            }
+        });
+        newNotes.setOnClickListener(this);
+        folder.setOnClickListener(this);
     }
 
     @Override
@@ -62,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public List<Note> getNotesList(){
+    public List<Note> getNotesList(){//get notes list from sqlite database -- Notesdb
         List<Note> noteList = new ArrayList<>();
         String sql = "select * from Notes";
         Cursor cursor = dbutil.rawQuery(sql, null);
@@ -81,6 +100,31 @@ public class MainActivity extends AppCompatActivity {
         return noteList;
     }
 
+    /*
+    TODO:
+    1. click newNotes to jump to EditActivity
+    2. click item once to get the context and edit it (update)
+    3. click item long time to remove the item or edit it (delete or update
+    4. input string to search notes, using like and % %
+    5. click folder to jump to ShowFolderActivity
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.new_note:{
+                TextView folder_name = (TextView)findViewById(R.id.folder_name);
+                EditActivity.actionStart(MainActivity.this, " ");
+//                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+//                startActivity(intent);
+            }break;
+            case R.id.folder:{
+
+            }break;
+            default:
+                break;
+        }
+    }
+
     public void RefreshNoteList(ArrayList<Map<String , String>> datalist, ListView listView){
         int size = datalist.size();
         if(size > 0){
@@ -88,4 +132,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 }
